f31f2d9dbdc3fa8ad550bcb929151feade38ea13
devmello <robertale45@gmail.com>
2026-01-14 17:12:41 -0300

    Criando a controller

---
+++ src/main/java/dev/mello/cadastroninjas/Controller.java
@@ -0,0 +1,21 @@
+package dev.mello.cadastroninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Boas vindas sua Api está funcionando na rota boasvindas";
+    }
+
+
+
+
+
+}
