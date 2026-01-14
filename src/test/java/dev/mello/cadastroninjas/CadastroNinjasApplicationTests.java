3784483667854bb26322fd786137c6ac3bb2e660
devmello <robertale45@gmail.com>
2026-01-13 22:48:00 -0300

    first commit

---
+++ src/test/java/dev/mello/cadastroninjas/CadastroNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mello.cadastroninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
